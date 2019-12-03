6c8c1473944d2b7fee700ffd5525e149be8d6cab
Nicholas <16458785+NickKalar@users.noreply.github.com>
2019-12-03 08:28:52 -0800

    Added mutation and fitness functions. Created a for loop to compare child to parent. Formatting added to output.

--- EvoStrat.py
+++ EvoStrat.py
@@ -1,8 +1,7 @@
 import numpy
 import csv
+import random
 
-
-numpy.random.normal()
 data = []
 
 with open('data.csv') as csv_file:
@@ -10,5 +9,37 @@ with open('data.csv') as csv_file:
     for row in csv_reader:
         data.append(row)
 
-for row in data:
-    print(row)
+
+def mutate(parent):
+    child = []
+    for i in parent:
+        child.append(i + numpy.random.normal(0.0, 1.0))
+    return child
+
+
+def fitness(array):
+    sse = 0
+    for row in data:
+        x = float(row[0])
+        yd = float(row[1])
+        y = array[0] * pow(x, 3) + array[1] * pow(x, 2) + array[2] * x + array[3]
+        sse += pow((yd - y), 2)
+    return sse
+
+
+def main():
+    pop = []
+    for i in range(4):
+        pop.append(random.uniform(-20.0, 20.0))
+    print(f"Starting Values: {pop}")
+    for i in range(100):
+        newPop = mutate(pop)
+        newPopFitness = fitness(newPop)
+        popFitness = fitness(pop)
+        if newPopFitness < popFitness:
+            pop = newPop
+            popFitness = newPopFitness
+        print(f"Iteration {i+1}: Values {pop}, Fitness {popFitness}")
+    print(f"Ending Values: {pop}")
+
+main()
